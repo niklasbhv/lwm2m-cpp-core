1425b432364522928303cb8e6f9751f1677f03c5
Niklas Meyer <nik_mey@uni-bremen.de>
2024-11-10 02:51:06 +0100

    Add initial lwm2m structures

--- include/lwm2m/lwm2m_cpp_core.h
+++ include/lwm2m/lwm2m_cpp_core.h
@@ -17,4 +17,62 @@
 #ifndef LWM2M_CPP_CORE_INCLUDE_LWM2M_LWM2M_CPP_CORE_H_
 #define LWM2M_CPP_CORE_INCLUDE_LWM2M_LWM2M_CPP_CORE_H_
 
+#include <string>
+#include <map>
+#include <pugixml.hpp>
+
+namespace lwm2m {
+
+enum Type {
+    String,
+    Integer,
+    Float,
+    Boolean,
+    Opaque,
+    Time,
+    ObjectLink,
+    UndefinedType
+};
+
+enum Operations {
+    Read,
+    Write,
+    ReadWrite,
+    Execute,
+    UndefinedOperation
+};
+
+struct Resource {
+    std::string name;
+    Operations operations;
+    bool multiple_instances;
+    bool mandatory;
+    Type type;
+    std::string range_enumeration;
+    std::string units;
+    std::string description;
+
+    static Resource Parse(const pugi::xml_node& resource_node);
+    void Serialize();
+};
+
+struct Object {
+    std::string name;
+    std::string object_type;
+    std::string description_1;
+    std::string description_2;
+    int object_id;
+    std::string object_urn;
+    float lwm2m_version;
+    float object_version;
+    bool multiple_instances;
+    bool mandatory;
+    std::map<int, Resource> resources;
+
+    static Object Parse(const pugi::xml_node& object_node);
+    void Serialize();
+};
+
+}
+
 #endif //LWM2M_CPP_CORE_INCLUDE_LWM2M_LWM2M_CPP_CORE_H_
--- src/lwm2m_cpp_core.cpp
+++ src/lwm2m_cpp_core.cpp
@@ -13,3 +13,109 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
+#include "lwm2m/lwm2m_cpp_core.h"
+#include <pugixml.hpp>
+
+namespace lwm2m {
+
+Resource Resource::Parse(const pugi::xml_node& resource_node) {
+    Resource resource;
+
+    resource.name = resource_node.child("Name").value();
+
+    std::string operation = resource_node.child("Operations").value();
+    if (operation == "R") {
+        resource.operations = Read;
+    } else if (operation == "W") {
+        resource.operations = Write;
+    } else if (operation == "RW") {
+        resource.operations = ReadWrite;
+    } else if (operation == "E") {
+        resource.operations = Execute;
+    } else {
+        resource.operations = UndefinedOperation;
+    }
+
+    std::string multiple_instances = resource_node.child("MultipleInstances").value();
+    if (multiple_instances == "Single") {
+        resource.multiple_instances = false;
+    } else {
+        resource.multiple_instances = true;
+    }
+
+    std::string mandatory = resource_node.child("Mandatory").value();
+    if (mandatory == "Optional") {
+        resource.mandatory = false;
+    } else {
+        resource.mandatory = true;
+    }
+
+    std::string type = resource_node.child("Type").value();
+    if (type == "String") {
+        resource.type = String;
+    } else if (type == "Integer") {
+        resource.type = Integer;
+    } else if (type == "Float") {
+        resource.type = Float;
+    } else if (type == "Boolean") {
+        resource.type = Boolean;
+    } else if (type == "Opaque") {
+        resource.type = Opaque;
+    } else if (type == "Time") {
+        resource.type = Time;
+    } else if (type == "Objlnk") {
+        resource.type = ObjectLink;
+    } else {
+        resource.type = UndefinedType;
+    }
+
+    resource.range_enumeration = resource_node.child("RangeEnumeration").value();
+    resource.units = resource_node.child("Units").value();
+    resource.description = resource_node.child("Description").value();
+
+    return resource;
+}
+
+void Resource::Serialize() {
+
+}
+
+Object Object::Parse(const pugi::xml_node& object_node) {
+    Object object;
+
+    object.name = object_node.child("Name").value();
+    object.object_type = object_node.attribute("ObjectType").value();
+    object.description_1 = object_node.child("Description1").value();
+    object.description_2 = object_node.child("Description2").value();
+    object.object_id = atoi(object_node.child("ObjectID").value());
+    object.object_urn = object_node.child("ObjectURN").value();
+    object.lwm2m_version = atof(object_node.child("LWM2MVersion").value());
+    object.object_version = atof(object_node.child("ObjectVersion").value());
+
+    std::string multiple_instances = object_node.child("MultipleInstances").value();
+    if (multiple_instances == "Single") {
+        object.multiple_instances = false;
+    } else {
+        object.multiple_instances = true;
+    }
+
+    std::string mandatory = object_node.child("Mandatory").value();
+    if (mandatory == "Optional") {
+        object.mandatory = false;
+    } else {
+        object.mandatory = true;
+    }
+
+    for (const auto child_node : object_node.child("Resource").children()) {
+        object.resources[child_node.attribute("ID").as_int()] = Resource::Parse(child_node);
+    }
+
+    return object;
+}
+
+void Object::Serialize() {
+
+}
+
+}
